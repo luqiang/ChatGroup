8435c0bdba13b9073e4bf8370f5a3551204f6055
FindHao <find@findspace.name>
2015-12-04 21:43:38 +0800

    v0.0.5多线程tkinter遇到了问题

--- Client/Start.py
+++ Client/Start.py
@@ -1,9 +1,54 @@
 from tkinter import *
-import socket
-from tkinter import messagebox
-
-
+import socket,threading,_thread
+from queue import Queue,Empty
 TITLE_FONT = ("Arial", 12, "")
+
+
+
+def position(self):
+    self.master.withdraw()
+    self.screen_width = self.master.winfo_screenwidth()
+    self.screen_height = self.master.winfo_screenheight()
+    # self.master.resizable(False,False) # 固定尺寸，不可变
+    self.master.update_idletasks()   # 显示正常窗口的关键语句
+    self.master.deiconify()   # 重新显示
+    self.master.withdraw() # TK
+    curWidth = self.master.winfo_reqwidth() # get current width
+    curHeight = self.master.winfo_height() # get current height
+    scnWidth,scnHeight = self.master.maxsize() # get screen width and height
+    tmpcnf = '%dx%d+%d+%d'%(curWidth,curHeight,(scnWidth-curWidth)/2,(scnHeight-curHeight)/2)
+    self.master.geometry(tmpcnf)
+    self.master.deiconify()
+
+class ThreadTask(threading.Thread):
+
+    def __init__(self, queue, sock):
+        threading.Thread.__init__(self)
+        self.queue=queue
+        self.sock = sock
+
+    def run(self):
+        while 1:
+            try:
+                clientMsg = self.sock.recv(4096).decode('utf8')
+                if not clientMsg:
+                    continue
+                else:
+                    # print(clientMsg)
+                    self.queue.put(clientMsg)
+            except socket.timeout:
+                pass
+
+
+# def receiveMessage(container,sock):
+#     while 1:
+#         clientMsg = sock.recv(4096).decode("utf8")
+#         if not clientMsg:
+#             continue
+#         else:
+#             container.chatShow.insert(END,clientMsg)
+#             container.chatShow.yview_moveto(1.0)
+
 class ClientNet:
     def __init__(self,aHost,aPort):
         self.serverHost=aHost
@@ -22,12 +67,15 @@ class ClientNet:
             print("unable to connect")
             self.connected=False
 
+    def sendData(self,data):
+        self.clientSock.sendall(data.encode('utf8'))
 
 class ChatShow(Frame):
     """普通用户展示的"""
 
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+    def __init__(self, master, controller):
+        Frame.__init__(self, master)
+        self.pack()
         #scrollbar
         consoleShowScrollBar=Scrollbar(self)
         consoleShowScrollBar.pack(side=RIGHT,fill=Y)
@@ -36,33 +84,67 @@ class ChatShow(Frame):
         consoleShow['yscrollcommand'] = consoleShowScrollBar.set
         consoleShow.pack(expand=1,fill=BOTH)
         consoleShowScrollBar['command'] = consoleShow.yview()
-
+        position(self)
 
     # def receiverMessage(self):
     #     try:
     #         self.clientSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     #         self.clientSock.connect((self.local, self.port))
 
-class AdminSHow(Frame):
+class AdminShow(Frame):
     """admin用，根据客户端有关，如果只是普通用户，那么即便在客户端代码设置了也没用。"""
-    def __init__(self,parent,controller):
-        Frame.__init__(self,parent)
-        #scrollbar
-        self.consoleShowScrollBar=Scrollbar(self)
-        self.consoleShowScrollBar.pack(side=RIGHT,fill=Y)
-        #console:text and pictures
-        self.consoleShow = Listbox(self)
-        self.consoleShow['yscrollcommand'] = self.consoleShowScrollBar.set
-        self.consoleShowScrollBar['command'] = self.consoleShow.yview()
-        self.consoleShow.pack(expand=1,fill=Y)
-        self.ent=Entry(self)
-        self.ent.pack(expand=1,fill=Y)
+    def __init__(self,master,controller):
+        Frame.__init__(self,master)
+        self.controller = controller
+        self.pack()
+        self.frameLeftTop = Frame(self)
+        self.frameLeftMid = Frame(self)
+        self.frameLeftBottom = Frame(self)
+
+        self.scrollbar = Scrollbar(self.frameLeftTop)
+        self.chatShow = Listbox(self.frameLeftTop,width=70,height=18,yscrollcommand=self.scrollbar.set)
+        self.chatShow.yview_moveto(1.0)
+        self.scrollbar.config(command=self.chatShow.yview)
+        self.scrollbar.pack(side="right", fill=Y)
+        self.chatShow.pack(side="left")
+        self.frameLeftTop.pack()
+
+        # self.sendPic= Button(self.frameLeftMid)
+        # self.sendPic['text'] = '发送图片'
 
 
+        self.messageInput= StringVar()
+        self.messageSend = Entry(self.frameLeftBottom,textvariable=self.messageInput)
+        self.messageSend['width'] = 70
+        self.messageSend.bind('<Return>',self.sendMessage)
+        self.messageSend.pack(fill=X)
+        self.frameLeftBottom.pack()
 
+        position(self)
+        self.queue = Queue()
+        ThreadTask(self.queue,controller.net.clientSock).start()
+        self.master.after(100,self.processQueue())
+
+    def sendMessage(self,event):
+        tempMessage=self.messageSend.get().strip(" ")
+        if tempMessage:
+            self.controller.net.sendData(tempMessage)
+        else:
+            print("不能发送空消息")
+
+    def processQueue(self):
+        try:
+            msg = self.queue.get(0)
+            print(msg)
+            self.chatShow.insert(END, msg)
+            self.chatShow.yview_moveto(1.0)
+        except Empty:
+            self.master.after(500, self.processQueue())
+            pass
 class Log(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+    def __init__(self,master, controller):
+        Frame.__init__(self,master)
+        self.pack()
         lab1 = Label(self,text = "User:", font=TITLE_FONT)
         lab1.grid(row = 0,column = 0,sticky = W)
         self.ent1 = Entry(self)
@@ -83,6 +165,9 @@ class Log(Frame):
         button2 = Button(self,text = "注册",command =None, font=TITLE_FONT)
         button2.grid(row = 2,column = 1,sticky = E)
 
+        position(self)
+
+
 
     def log(self,sock,controller):
         """
@@ -95,13 +180,13 @@ class Log(Frame):
         data = sock.recv(2048).decode("utf8")
         print(data)
         if data:
+            self.ent2.delete(0,len(s2))
             if data=='1':
-                controller.showChatShow()
+                controller.showChatShow(self.master)
             elif data== '2':
-                controller.showAdminShow()
+                controller.showAdminShow(self.master)
             else:
-                self.showServerState['text']='用户名或密码错误'
-        self.ent2.delete(0,len(s2))
+                self.showServerState['text']= '用户名或密码错误'
     def register(self):
         self.destory()
         return True
@@ -110,25 +195,16 @@ class Log(Frame):
         self.showServerState['text']=text
 
 
-class Client(Tk):
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        self.container = Frame(self)
-        self.container.pack(side="top", fill="both", expand=True)
-        self.container.grid_rowconfigure(0, weight=1)
-        self.container.grid_columnconfigure(0, weight=1)
-        self.frames = {}
-        # for F in (Log, ChatShow):
-        #     frame = F(container, self)
-        #     self.frames[F] = frame
-        #     frame.grid(row=0, column=0, sticky='nsew')
-        # self.show_frame(Log)
-        #加载登录窗口
-
+class Client():
+    def __init__(self):
+        self.root=Tk()
         self.net=ClientNet("192.168.1.133",'5000')
         self.connectedSuccess=False
-        self.showLog()
+        self.app= Log(self.root,self)
+        #检测服务器连接
+        self.tryConnect(self.app)
 
+        self.root.mainloop()
 
 
     def tryConnect(self,showTextView):
@@ -138,39 +214,22 @@ class Client(Tk):
         else:
             showTextView.showLogInfor("服务器连接失败")
 
-    def showLog(self):
-        tempFrame=Log(self.container,self)
-        tempFrame.grid(row=0, column=0, sticky='nsew')
-        tempFrame.tkraise()
-        self.updateFrame()
-        self.tryConnect(tempFrame)
+    def showChatShow(self,temproot):
+        temproot.destroy()
+        temproot=Tk()
+        temproot=ChatShow(temproot,self)
 
-    def showChatShow(self):
-        tempFrame=ChatShow(self.container,self)
-        tempFrame.grid(row=0, column=0, sticky='nsew')
-        tempFrame.tkraise()
-        self.geometry("150x500")
-        self.updateFrame()
-    def showAdminShow(self):
-        tempFrame=AdminSHow(self.container,self)
-        tempFrame.grid(row=0, column=0, sticky='nsew')
-        tempFrame.tkraise()
-        # self.geometry("150x500")
-        self.updateFrame()
-    def updateFrame(self):
-        """主要是用来移动界面到屏幕中央"""
-        self.update() # update window ,must do
-        curWidth = self.winfo_reqwidth() # get current width
-        curHeight = self.winfo_height() # get current height
-        scnWidth,scnHeight = self.maxsize() # get screen width and height
-        tmpcnf = '%dx%d+%d+%d'%(curWidth,curHeight,(scnWidth-curWidth)/2,(scnHeight-curHeight)/2)
-        self.geometry(tmpcnf)
+    def showAdminShow(self,temproot):
+        temproot.destroy()
+        temproot=Tk()
+        tempFrame=AdminShow(temproot,self)
+        tempFrame.mainloop()
 
-def on_closing():
-    if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        app.net.clientSock.close()
-        app.destroy()
 
-if __name__ == "__main__":
-    app = Client()
-    app.mainloop()
+        # _thread.start_new_thread(receiveMessage,(tempFrame, self.net.clientSock))
+        # receiveMessage(tempFrame,self.net.clientSock)
+
+
+
+
+runClient=Client()
--- Server/server.py
+++ Server/server.py
@@ -5,7 +5,7 @@ from user import *
 def broadcaseData(sock, message):
     for socket in CONNECTIONLIST:
         print(message)
-        if socket != serverSocket and socket != sock:
+        if socket != serverSocket:
             try:
                 socket.send(message.encode("utf8"))
             except:
@@ -33,7 +33,7 @@ def readUserInfoFromFile():
         users[tempuser[0]] = tempuser[1:4]
     return 0
 
-def checkLog(data):
+def checkLog(data,sock):
     """
     @return value False:用户名或者密码错误 True:成功
     :param data: 传入参数data的格式是userName@key,解出name和key，匹配数据库中的
@@ -45,7 +45,11 @@ def checkLog(data):
         print("no user")
         return False
     if users[aName][0] == aKey:
+        loggers[sock].name=aName
+        loggers[sock].logged= True
+
         if(users[aName][1]=='1'):
+            loggers[sock].isAdmin = True
             return 2
         else:
             return 1
@@ -60,7 +64,6 @@ def checkRegister(data):
 if __name__ == "__main__":
     # 建立连接的人
     loggers={}
-    admins=[]
     #从硬盘读取的已经注册的用户信息
     users={}
     #读用户信息
@@ -114,15 +117,17 @@ if __name__ == "__main__":
                     if not data:
                         raise Exception("null data")
                     #如果有name,那么肯定已经登录了，直接发送消息就可以
-                    if loggers[sock].name in admins:
+                    if loggers[sock].isAdmin:
                         # 先不管图片了,只处理文字消息
                         broadcaseData(sock,data)
+                        print("admin say"+data)
                     else:
                         if sock in loggers:
                             #尝试登录
                             if data[0:2]=='1_':
                                 print("logging")
-                                temp1=checkLog(data[2:])
+                                temp1=checkLog(data[2:],sock)
+
                                 sock.sendall(str(temp1).encode("utf8"))
                             #注册
                             elif data[0:2]=='2_':
